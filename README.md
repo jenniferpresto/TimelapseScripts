e3bf8fd3520b8f2071ab623ff8483d44775260de
Jennifer Presto <sandlappernyc@yahoo.com>
2015-07-19 23:16:38 -0400

    added more info to readme; smoothed movie script

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ###Timelapse scripts
 
 Simplest--possibly the jenkiest--timelapse system ever created. More detailed readme to come.
+
+* **simpleCam.sh**: Bash script set to run as cron job on Raspberry Pi
+* **autoDownload.sh**: Logs into Raspberry Pi and downloads all pictures taken the prior day.
+* **makeMovie.py**: Stitches still images together to mp4.
--- makeMovie.py
+++ makeMovie.py
@@ -6,17 +6,20 @@ import datetime
 # FFMPEG command--still dealing with deprecated pixel format issues:
 #ffmpeg -r 24 -i img%05d.jpg -vf scale=640:480 -c:v libx264 -pix_fmt yuv420p testMovie.mp4
 
-origDir = 'imgFileTest/'
-tmpDir = 'tmpFiles/'
+logDir = '../timelapseScratch/'
+imageDir = '../timelapseScratch/imgFiles/'
+tmpDir = '../timelapseScratch/tmpFiles/'
 
-ffmpegCmd = ['ffmpeg', '-r', '24', '-i', tmpDir + 'img%05d.jpg', '-vf', 'scale=640:480', '-c:v', 'libx264', '-pix_fmt', 'yuv420p', 'testMovie.mp4']
+ffmpegCmd = ['ffmpeg', '-r', '24', '-i', tmpDir + 'img%05d.jpg', '-vf', 'scale=640:480', '-c:v', 'libx264', '-pix_fmt', 'yuv420p', logDir + 'testMovie.mp4']
 
 now = datetime.datetime.now()
 dateString = now.strftime("%Y_%m_%d__%H%M")
-logFile = 'log' + dateString + '.txt'
 
+# set up logging
+logFile = logDir + 'log' + dateString + '.txt'
 logging.basicConfig(filename = logFile, level=logging.DEBUG)
 print('Logging file information to ' + logFile)
+logging.info('Time of operation: ' + now.strftime("%Y/%m/%d, %H:%M:%S"))
 logging.info('\tOld filename to new filename:')
 filecount = 0
 
@@ -27,21 +30,21 @@ if not os.path.exists(tmpDir):
 	os.makedirs(tmpDir)
 
 # run through existing files
-for fn in os.listdir(origDir):
+for fn in os.listdir(imageDir):
 	if fn.endswith ('.jpg'):
+		# TODO: test for brightness
+
 		print ("Number " + str(filecount) + ": " + fn)
+		# save the path name for original image file
+		origName = imageDir + fn
+
 		# create 5-digit numeric names
 		tmpName = str(filecount).zfill(5)
 		tmpName = 'img' + tmpName + '.jpg'
 
-		origName = origDir + fn
-
 		# log tmpNames with original names
 		print('temporary name: ' + tmpName)
 		logging.info('\t' + fn + '\t\t' + tmpName)
-
-		# # create dictionary
-		# fileDictionary[tmpName] = fn
 
 		# create symbolic links using numeric names
 		dest = tmpDir + '/' + tmpName
@@ -54,14 +57,9 @@ for fn in os.listdir(origDir):
 # Do FFMPEG magic
 subprocess.call(ffmpegCmd)
 
+# unlink the symbolic links
+for link in os.listdir(tmpDir):
+	os.unlink(tmpDir + link)
 
-
-
-# # change filenames back
-# for entry in fileDictionary:
-# 	print(fn)
-# 	print(fileDictionary[entry])
-
-# for keyname in fileDictionary.keys():
-# 	print(keyname)
-# 	os.rename(keyname, fileDictionary[keyname])
+# delete empty folder
+os.rmdir(tmpDir)
