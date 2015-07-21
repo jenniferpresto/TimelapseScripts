1a76123c84a983a15c89bfd915df0f2b1099c6d6
Jennifer Presto <sandlappernyc@yahoo.com>
2015-07-20 23:47:41 -0400

    added test for brightness and skips frames that are too dark; adjusted logging

--- makeMovie.py
+++ makeMovie.py
@@ -1,65 +1,106 @@
+import sys
 import subprocess
 import logging
 import os
 import datetime
+from PIL import Image
+from PIL import ImageStat
+
 
 # FFMPEG command--still dealing with deprecated pixel format issues:
 #ffmpeg -r 24 -i img%05d.jpg -vf scale=640:480 -c:v libx264 -pix_fmt yuv420p testMovie.mp4
 
-logDir = '../timelapseScratch/'
-imageDir = '../timelapseScratch/imgFiles/'
-tmpDir = '../timelapseScratch/tmpFiles/'
+def main():
+	logDir = '../timelapseScratch/'
+	imageDir = '../timelapseScratch/imgFiles/'
+	tmpDir = '../timelapseScratch/tmpFiles/'
 
-ffmpegCmd = ['ffmpeg', '-r', '24', '-i', tmpDir + 'img%05d.jpg', '-vf', 'scale=640:480', '-c:v', 'libx264', '-pix_fmt', 'yuv420p', logDir + 'testMovie.mp4']
+	darknessThreshold = 30.0
 
-now = datetime.datetime.now()
-dateString = now.strftime("%Y_%m_%d__%H%M")
+	now = datetime.datetime.now()
+	dateString = now.strftime("%Y_%m_%d_%H%M")
+	timeString = now.strftime("%Y/%m/%d, %H:%M:%S")
 
-# set up logging
-logFile = logDir + 'log' + dateString + '.txt'
-logging.basicConfig(filename = logFile, level=logging.DEBUG)
-print('Logging file information to ' + logFile)
-logging.info('Time of operation: ' + now.strftime("%Y/%m/%d, %H:%M:%S"))
-logging.info('\tOld filename to new filename:')
-filecount = 0
+	ffmpegCmd = ['ffmpeg', '-r', '24', '-i', tmpDir + 'img%05d.jpg', '-vf', 'scale=640:480', '-c:v', 'libx264', '-pix_fmt', 'yuv420p', logDir + dateString + 'Movie.mp4']
 
-fileDictionary = {}
 
-# create temporary folder for symbolic links
-if not os.path.exists(tmpDir):
-	os.makedirs(tmpDir)
+	# set up logging
+	logFile = logDir + dateString + 'Log.txt'
+	logging.basicConfig( filename = logFile, level=logging.DEBUG )
+	print('####Logging file information to ' + logFile)
+	logging.info('Beginning operation: ' + timeString)
+	logging.info('Original filename\tFrame\t\tBrightness')
+	filecount = 0
 
-# run through existing files
-for fn in os.listdir(imageDir):
-	if fn.endswith ('.jpg'):
-		# TODO: test for brightness
+	# create temporary folder for symbolic links
+	if not os.path.exists(tmpDir):
+		os.makedirs(tmpDir)
 
-		print ("Number " + str(filecount) + ": " + fn)
-		# save the path name for original image file
-		origName = imageDir + fn
+	# run through existing files
+	print('####Analyzing images')
+	for fn in os.listdir(imageDir):
 
-		# create 5-digit numeric names
-		tmpName = str(filecount).zfill(5)
-		tmpName = 'img' + tmpName + '.jpg'
+		# Skip files that aren't jpgs
+		if not fn.endswith('.jpg'):
+			# print('Not an image file: ' + fn)
+			continue
 
-		# log tmpNames with original names
-		print('temporary name: ' + tmpName)
-		logging.info('\t' + fn + '\t\t' + tmpName)
+		imageFilePath = imageDir + fn
 
-		# create symbolic links using numeric names
-		dest = tmpDir + '/' + tmpName
-		os.symlink(os.path.abspath(origName), os.path.abspath(dest))
+		# Skip image files that are too dark
+		level = testBrightness(imageFilePath)
+		sys.stdout.write('#')
+		sys.stdout.flush()
+		if level < darknessThreshold:
+			logging.info(fn + '\tToo dark' + '\tLevel: ' + str(level))
+			continue
+
+		# create symbolic links
+		createLinks(imageFilePath, tmpDir, filecount)
+		logging.info(fn + '\timg' + str(filecount).zfill(5) + '\tLevel: ' + str(level))
 
 		filecount += 1
-	else:
-		print ("Not a jpg file: " + fn)
 
-# Do FFMPEG magic
-subprocess.call(ffmpegCmd)
+	# Do FFMPEG magic
+	print('\n####Building video: Num frames: ' + str(filecount))
+	subprocess.call(ffmpegCmd)
 
-# unlink the symbolic links
-for link in os.listdir(tmpDir):
-	os.unlink(tmpDir + link)
+	# Remove temporary links and directory
+	print('####Cleaning up')
+	cleanUpLinks(tmpDir)
 
-# delete empty folder
-os.rmdir(tmpDir)
+	endtime = datetime.datetime.now()
+	elapsed = endtime - now
+	logging.info('Ending operation: ' + datetime.datetime.now().strftime("%Y/%m/%d, %H:%M:%S"))
+	logging.info('Elapsed time: ' + str(elapsed))
+	print('####Complete.')
+
+# Create symbolic links for the image files that work
+def createLinks(filepath, tempdirectory, framenum):
+	# create 5-digit numeric names
+	tmpName = str(framenum).zfill(5)
+	tmpName = 'img' + tmpName + '.jpg'
+
+	# create symbolic links using numeric names
+	dest = tempdirectory + tmpName
+	os.symlink(os.path.abspath(filepath), os.path.abspath(dest))
+
+# Test overall brightness of each image
+# http://stackoverflow.com/questions/3490727/what-are-some-methods-to-analyze-image-brightness-using-python
+def testBrightness( img_file ):
+	img = Image.open(img_file).convert('L')
+	stat = ImageStat.Stat(img)
+	return stat.rms[0]
+
+# Erase symbolic links and temporary directory
+def cleanUpLinks(tempdirectory):
+	# unlink the symbolic links
+	for link in os.listdir(tempdirectory):
+		os.unlink(tempdirectory + link)
+
+	# delete empty folder
+	os.rmdir(tempdirectory)
+
+
+if __name__ == "__main__":
+	main()
